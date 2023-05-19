9daa6525b7d35d36b448b76a6515dae8e7299cb9
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-05-19 15:03:41 -0500

    Add project files.

---
+++ Demo0720223B/Demo0720223B/Interfaces/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo0720223B.Interfaces
+{
+    public interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
---
+++ Demo0720223B/Demo0720223B/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Demo0720223B.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo0720223B
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpeak_Clicked(object sender, EventArgs e)
+        {
+
+         DependencyService.Get<ITextToSpeech>().Speak(txtMessage.Text);
+            
+        }
+    }
+}
